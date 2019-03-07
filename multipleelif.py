c5b4074b7f5c3b49b1ea395b0e576eb33312d7fe
@Maheshnagakumar <maheshnagakumar123@gmail.com>
2019-03-07 12:55:29 +0000

    multiple elif

---
+++ multipleelif.py
@@ -0,0 +1,11 @@
+color=input()
+if color=="purple":
+    print("Execellent")
+elif color=="pink":
+    print("you are cool bro")
+elif color=="blue":
+    print("nice choice")
+elif color=="green":
+    print("you are great")
+else:
+    print("Monster")	
